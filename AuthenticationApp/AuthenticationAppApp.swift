194a9239a5a74162f4244e6ec3e2c91a5325c6ec
Max Alva Garcia <alva85@gmail.com>
2022-12-13 16:29:37 -0500

    Initial Commit

---
+++ AuthenticationApp/AuthenticationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationAppApp.swift
+//  AuthenticationApp
+//
+//  Created by Max Alva on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
